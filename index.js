6148beed472a96fc53b08a67c52f4669903b1b3b
Catherine Bronzo <cbronzo@mac.com>
2019-03-17 22:54:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,8 @@
 //   this.name = name
 //   this.homeState = homeState
 //   this.training = training
-BoardMember.prototype.veto
-//
-
-//   this.veto = function(){
-//     return 'No, I must disagree'
+BoardMember.prototype.veto = function(){
+    return 'No, I must disagree'
 //   }
 //   this.approve = function(){
 //     return 'You can do that!'
