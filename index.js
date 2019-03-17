41c41e8f1e39d13f23caff86be34cb93b62eaf3b
Catherine Bronzo <cbronzo@mac.com>
2019-03-17 22:55:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 //   this.name = name
 //   this.homeState = homeState
 //   this.training = training
-BoardMember.prototype.veto = function(){
-    return 'No, I must disagree'
+//
+//   this.veto = function(){
+//     return 'No, I must disagree'
 //   }
 //   this.approve = function(){
 //     return 'You can do that!'
 //   }
-
 //   this.doCharity = function(){
 //     return "I like to help people."
 //   }
@@ -21,3 +21,13 @@ BoardMember.prototype.veto = function(){
 //     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 //   }
 // }
+
+
+
+function BoardMember(name, homeState, training){
+  this.name = name
+  this.homeState = homeState
+  this.training = training
+
+BoardMember.prototype.veto = function(){
+    return 'No, I must disagree'
